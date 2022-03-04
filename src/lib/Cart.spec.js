4340a003b01b3c5c3da6b690e7d4785c994c0edd
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-04 19:57:43 -0300

    creating cart tests

---
+++ src/lib/Cart.spec.js
@@ -0,0 +1,8 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  it('should return 0 when getTotal() is executed in a newly created instance', () => {
+    const cart = new Cart();
+    expect(cart.getTotal()).toEqual(0);
+  });
+});
