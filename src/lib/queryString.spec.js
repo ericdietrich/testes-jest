f53cbd960769ca9f5afa6638f4f83b7b101a032e
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-04 12:35:13 -0300

    add new tests to queryString

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,4 +9,27 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Eric&profession=developer');
   });
+
+  it('should create a valid query string even when array is passed as value', () => {
+    const obj = {
+      name: 'Eric',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Eric&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Eric',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
