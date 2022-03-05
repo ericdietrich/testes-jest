c2552868bcd844e7cd8e5014e917c057380fe2c4
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-04 21:20:53 -0300

    adding methods to Cart

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -1,8 +1,27 @@
 import Cart from './Cart';
 
 describe('Cart', () => {
+  let cart;
+  //before each test(it)
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
   it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    const cart = new Cart();
     expect(cart.getTotal()).toEqual(0);
+  });
+
+  it('should multiply quantity and price and receive the total amount', () => {
+    const item = {
+      product: {
+        title: 'Adidas running shoes - men',
+        price: 35388, // 353.88 | R$383,88
+      },
+      quantity: 2, // 70776
+    };
+
+    cart.add(item);
+
+    expect(cart.getTotal()).toEqual(70776);
   });
 });
