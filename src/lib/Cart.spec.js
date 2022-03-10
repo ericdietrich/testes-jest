93fb2bb941551cc8a6c9c764d4bc09ca9d1ea115
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-10 19:27:58 -0300

    add new tests to Cart.js

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,6 +2,15 @@ import Cart from './Cart';
 
 describe('Cart', () => {
   let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388, // 353.88 | R$383,88
+  }
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872, // 418.72 | R$418,72
+  }
+
   //before each test(it)
   beforeEach(() => {
     cart = new Cart();
@@ -13,15 +22,42 @@ describe('Cart', () => {
 
   it('should multiply quantity and price and receive the total amount', () => {
     const item = {
-      product: {
-        title: 'Adidas running shoes - men',
-        price: 35388, // 353.88 | R$383,88
-      },
+      product,
       quantity: 2, // 70776
     };
 
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(70776);
+  });
+
+  it('should ensure no more than one product exists at a time', () => {
+    cart.add({
+      product,
+      quantity: 2, 
+    });
+
+    cart.add({
+      product,
+      quantity: 1, 
+    });
+
+    expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it('should update total when a product gets included and then removed', () => {
+    cart.add({
+      product,
+      quantity: 2, 
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1, 
+    });
+
+    cart.remove(product)
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });
