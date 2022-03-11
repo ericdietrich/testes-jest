b04e2ab030942015dadfbcbb528c3574d3d9ea6e
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-11 16:49:03 -0300

    add summary and checkout tests

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -77,5 +77,30 @@ describe('Cart', () => {
 
       expect(cart.checkout()).toMatchSnapshot();
     });
+
+    it('should return an object with the total and the list of the items when summary is called', () => {
+      cart.add({
+        product,
+        quantity: 2, 
+      });
+  
+      cart.add({
+        product: product2,
+        quantity: 1, 
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal()).toBeGreaterThan(0);
+    });
+
+    it('should should reset the cart when checkout is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 3, 
+      });
+      cart.checkout()
+
+      expect(cart.getTotal()).toEqual(0);
+    });
   });
 });
