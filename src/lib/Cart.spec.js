ae1c0225c8b3b045b32ac55ed331208a91bd1115
Eric Dietrich <eric.dietrich@alpes.one>
2022-03-28 13:26:32 -0300

    add discount for quantities

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -120,5 +120,20 @@ describe('Cart', () => {
       expect(cart.getTotal()).toEqual(74315);
 
     });
+
+    it('should apply quantity discount for even quantities', () => {
+      const condition = {
+        quantity: 2
+      }
+
+      cart.add({
+        product,
+        condition,
+        quantity: 4,
+      })
+
+      expect(cart.getTotal()).toEqual(70776);
+
+    });
   })
 });
